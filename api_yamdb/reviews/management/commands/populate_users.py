b20a533cec1f9861699f463860884b08e8a7f45f
KitKat-ru <althome195@gmail.com>
2022-07-16 18:56:54 +0300

    test_project3

--- api_yamdb/reviews/management/commands/populate_users.py
+++ api_yamdb/reviews/management/commands/populate_users.py
@@ -21,8 +21,7 @@ class Command(BaseCommand):
 
     def get_csv_file(self, filename):
         app_path = self.get_current_app_path()
-        file_path = os.path.join(app_path, "data", filename)
-        return file_path
+        return os.path.join(app_path, "data", filename)
 
     def clear_model(self):
         try:
