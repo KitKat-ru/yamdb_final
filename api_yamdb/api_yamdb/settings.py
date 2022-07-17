283d30dd9d1205d93cb24b38a6d30390ea0447bc
KitKat-ru <althome195@gmail.com>
2022-07-18 01:23:44 +0300

    deploy2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [BASE_DIR + '/api/static/']
 
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [BASE_DIR + '/api/static/']
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 AUTH_USER_MODEL = 'users.User'
